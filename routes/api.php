e92ac4606e08a0f62ffb8e11c3db95da00bc5b3c
Jandrei Marques <jandrei.marques@solution4fleet.com.br>
2024-10-23 19:29:00 -0300

    rotas na api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\BrasilApiController;
+use App\Http\Controllers\IbgeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/teste', function () {
+    return 'Rota de teste funcionando!';
+});
+Route::get('/brasilapi', [BrasilApiController::class, 'index']);
+Route::get('/brasilapi/municipios/{estadoId?}', [BrasilApiController::class, 'municipios']);
+Route::get('/ibge', [IbgeController::class, 'index']);
+Route::get('/ibge/municipios/{estadoId?}', [IbgeController::class, 'municipios']);
