e32823cf3287e940ee8dc6ff94767a6b79c07caa
Jandrei Marques <jandrei.marques@solution4fleet.com.br>
2024-10-23 17:32:14 -0300

    Enviando app

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://idez-7ed2cac2f70f.herokuapp.com/'),
 
     'asset_url' => env('ASSET_URL'),
 
